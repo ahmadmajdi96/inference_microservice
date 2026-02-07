aa925e7803c572ff86e62ae20c20ca90fd9d717f
Ahmad Salameh <ahmad.majdi96@gmail.com>
2026-02-07 23:21:21 +0300

    cors fix

--- app/main.py
+++ app/main.py
@@ -1,10 +1,17 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 from app.api.routes import router
 from app.core.config import settings
 from app.services.models import load_models
 
 app = FastAPI(title=settings.app_name)
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_methods=["GET", "POST", "OPTIONS"],
+    allow_headers=["Content-Type"],
+)
 
 
 @app.on_event("startup")
